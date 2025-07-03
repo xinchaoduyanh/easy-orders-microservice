32ef5952a0191407021c023f36947516f78d3237
foq2 <duyanh19122k3@gmail.com>
2025-07-03 11:29:50 +0700

    Update app.module.ts

--- payments-service/src/app.module.ts
+++ payments-service/src/app.module.ts
@@ -4,10 +4,6 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PaymentsModule } from './payments/payments.module'; // Import PaymentsModule
 import { ConfigModule } from '@nestjs/config'; // Import ConfigModule
-import { HttpExceptionFilter } from 'src/shared/filter/http_exception.filter';
-import { APP_FILTER, APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core';
-import CustomZodValidationPipe from 'src/shared/pipes/custom-zod-validation.pipe';
-import { ZodSerializerInterceptor } from 'nestjs-zod';
 
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true }), PaymentsModule],
